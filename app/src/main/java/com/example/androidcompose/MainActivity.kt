d3aaaccbd87f0865bfea0df9a83a12f947ee6a13
MatheusOlca <matheus.caldas2@etec.sp.gov.br>
2022-10-31 08:09:15 -0300

    Layout

--- app/src/main/java/com/example/androidcompose/MainActivity.kt
+++ app/src/main/java/com/example/androidcompose/MainActivity.kt
@@ -3,31 +3,49 @@ package com.example.androidcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.androidcompose.ui.theme.AndroidComposeTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard("Galdino")
+            MessageCard(Message("Bolsominion", "Lula ladrão, roubou meu coração!"))
         }
     }
 }
 
+data class Message(val author:String, val body:String)
+
 @Composable
-fun MessageCard(name:String) {
-    Text(text = "Hello $name!")
+fun MessageCard(msg:Message) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image (
+            painter = painterResource(id = R.drawable.icon),
+            contentDescription = "O Meu Malvado Favorito são os amigos que fazemos no caminho",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Column {
+            Text(text = msg.author)
+            Spacer(modifier = Modifier.width(4.dp))
+            Text(text = msg.body)
+        }
+    }
 }
 
 @Preview
 @Composable
 fun PreviewMessageCard() {
-    MessageCard("Galdino")
+    MessageCard(Message("Petista", "Bolsonaro Genocída!"))
 }
